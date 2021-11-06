c20456310f536a9109af0d571d7dc9f7855642b9
Lxiaojiao666 <727991861@qq.com>
2021-11-06 17:05:38 +0800

    PageObject

---
+++ pageObject_learn/hogwarts.py
@@ -0,0 +1,8 @@
+class Hogwarts:
+    def title(self):
+        #定位title，并点击
+        pass
+
+    def get_text(self):
+        #获取页面内容
+        pass
---
+++ pageObject_learn/main.py
@@ -0,0 +1,21 @@
+from pageObject_learn.hogwarts import Hogwarts
+
+
+class Main:
+    def send_keys(self):
+        #定位输入框，输入内容
+        pass
+
+    def click(self):
+        #点击 百度一下 按钮
+        pass
+
+    def title(self):
+        #定位 小标题，并且点击
+        pass
+
+    def click_first_link(self):
+        #定位第一个链接，并点击
+        #将页面跳转到新的page
+        return Hogwarts()
+        pass
